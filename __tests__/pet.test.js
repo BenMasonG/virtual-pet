f20527b93bdf356fd6f203de63e99d4687545d63
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-04-29 12:56:09 +0100

    growUp function added to Pet.prototype. A test has been created to ensure this function behaves as expected

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,18 +1,36 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    it('tests that I can name a new Pet', () => {
-      const pet = new Pet ('Fido')
+  it('tests that I can name a new Pet', () => {
+    const pet = new Pet ('Fido')
 
-      expect(pet.name).toEqual('Fido');
-    });
+    expect(pet.name).toEqual('Fido');
   });
+});
 
 describe('constructor', () => {
-    it('tests that I can name a new Pet', () => {
-      const pet = new Pet ('Bugs')
+  it('tests that I can name a new Pet', () => {
+    const pet = new Pet ('Bugs')
 
-      expect(pet.name).toEqual('Bugs');
-    });
+    expect(pet.name).toEqual('Bugs');
   });
+});
+
+describe('constructor', () => {
+  it('tests that when a new pet is created it has an age of 0', () => {
+    const pet = new Pet ('Bugs')
+
+    expect(pet.age).toEqual(0);
+  });
+});
   
+describe('constructor', () => {
+  it('tests that when the growUp function is called on a pet it\'s age increases by 1', () => {
+    const pet = new Pet ('Bugs')
+    pet.growUp()
+    expect(pet.age).toBe(1);
+
+    pet.growUp()
+    expect(pet.age).toBe(2);
+  });
+});
--- src/pet.js
+++ src/pet.js
@@ -1,7 +1,13 @@
 
-function Pet(name, age) {
+function Pet(name) {
     this.name = name;
-    this.age = age;
+    this.age = 0;
 };
+
+
+Pet.prototype.growUp = function () {
+    this.age = this.age + 1;
+    console.log(this.age)
+}
 
 module.exports = Pet;
