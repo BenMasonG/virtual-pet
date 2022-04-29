eff833a02fe799545d9f958e1a115b8445ef120c
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-04-29 12:28:43 +0100

    Pet constructor to make a new pet with a name completed. Test to test this function also completed

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,8 +1,10 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    it('returns an object', () => {
-      expect(new Pet('Fido')).toBe({name: 'Fido'});
+    it('returns an instance of a new Pet', () => {
+      expect(new Pet('Fido')).toEqual({name: 'Fido'});
+      expect(new Pet('Bugs')).toEqual({name: 'Bugs'});
+      expect(new Pet('Matt')).toEqual({name: 'Matt'});
     });
   });
   
